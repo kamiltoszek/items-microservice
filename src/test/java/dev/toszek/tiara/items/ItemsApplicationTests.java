3e22124f07e60adbed22ee08726b1fdf1f131e71
Kamil Toszek <kamil.toszek.external@zf.com>
2023-07-08 17:19:55 +0200

    init: spring initializer generated project

---
+++ src/test/java/dev/toszek/tiara/items/ItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.toszek.tiara.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
